436aada01b63467571e3ca9e627ec2ec88842402
hatet-lucas <lcs.hatet@gmail.com>
2025-03-02 15:40:33 +0100

    app.tsx modif

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="" element={<Home />} />
         </Routes>
     </Router>
   );
