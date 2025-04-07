da1d4f20d19df00903482f5ff38576a8ec133203
lucas-hatet <lcs.hatet@gmail.com>
2025-04-07 17:57:20 +0200

    push

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,17 @@
-import '../style/Navbar.css';
+import './Navbar.css';
 import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/presentation">Présentation</Link>
-      <Link to="/parcours">Parcours</Link>
-      <Link to="/competences">Compétences</Link>
-      <Link id="btn-home" to="/">LH</Link>
-      <Link to="/projets">Projets</Link>
-      <Link to="/interets"><div>Intérêts</div></Link>
-      <Link to="/contacts">Contacts</Link>
+      <Link to="/presentation"><p>Présentation</p></Link>
+      <Link to="/parcours"><p>Parcours</p></Link>
+      <Link to="/projets"><p>Projets</p></Link>
+      <Link id="btn-home" to="/"><p>LH</p></Link>
+      <Link to="/competences"><p>Compétences</p></Link>
+      <Link to="/interets"><p>Intérêts</p></Link>
+      <Link to="/contacts"><p>Contacts</p></Link>
     </nav>
   );
 };
