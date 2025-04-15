cd6e2cac0abeeb8187695860dad5d8a3e43007a6
lucas-hatet <lcs.hatet@gmail.com>
2025-04-15 16:20:05 +0200

    parcours

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home.tsx";
 import Presentation from "./pages/Presentation.tsx";
+import Parcours from "./pages/Parcours.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 import Navbar from "./components/Navbar.tsx";
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/presentation" element={<Presentation />} />
+        <Route path="/parcours" element={<Parcours />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
---
+++ src/pages/Parcours.tsx
@@ -0,0 +1,16 @@
+import '../style/Presentation.css';
+import { Link } from "react-router-dom";
+import Button from '../components/Button.tsx';
+
+const Parcours = () => {
+  return (
+    <div className="container">
+      <section className="column">
+      </section>
+      <section className="column">
+      </section>
+    </div>
+  );
+};
+
+export default Parcours;
